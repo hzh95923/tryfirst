537e549e36ba5866e7e9d2b6b4faeea85493d23e
hzh95923 <56847679@qq.com>
2016-12-03 23:36:30 +0800

    push

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const bodyparser=require('body-parser');
 const session = require('express-session');
 const flash = require('connect-flash');
 
-push2
+push
 var app=express();
 app.set('trust proxy', 1); // trust first proxy 
 app.use(session({
