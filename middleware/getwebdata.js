5a34453d2f0b69072de1d33f1cda3931f241d5b9
hzh95923 <56847679@qq.com>
2016-12-05 22:56:24 +0800

    晚上

--- middleware/getwebdata.js
+++ middleware/getwebdata.js
@@ -1,26 +1,33 @@
 const cheerio = require('cheerio');
 const superagent = require('superagent');
+const async=require('async');
 
-module.exports=function(url,query,callback){
-  superagent.get(url)
-  	.query(query)
-    .end(function (err, sres) {
-      if (err) {
-        callback(err,null);
-      }
-      var $ = cheerio.load(sres.text);
-      var items = {};
-      $('.Product_grid').each(function (index, element) {
-        var $element = $(element);
-        items={
-          href: $element.find('.showItems a').attr("href"),
-          title: $element.find('.showItems a img').attr("alt"),
-          imgurl:$element.find('.showItems a img').attr("src"),
-          reviewUrl:$element.find('.review a').attr("href"),
-          reviewnum:$element.find('.review a').text(),
-          id:$element.find('.hdnPid').val(),
-        };
-      });
-      callback(items);
-    });
+module.exports = function(url, items, callback) {
+	async.mapSeries(items, function(item, callback) {
+		superagent.get(url)
+		.query({'keyword':item[0]})
+		.end(function(err, sres) {
+			if(err) {
+				callback(err, null);
+			}
+			var $ = cheerio.load(sres.text);
+			var items = {};
+			$('.Product_grid').each(function(index, element) {
+				var $element = $(element);
+				items = {
+					href: $element.find('.showItems a').attr("href"),
+					title: $element.find('.showItems a img').attr("alt"),
+					imgurl: $element.find('.showItems a img').attr("src"),
+					reviewUrl: $element.find('.review a').attr("href"),
+					reviewnum: $element.find('.review a').text(),
+					id: $element.find('.hdnPid').val(),
+				};
+			});
+			callback(null, items);
+		});
+
+	}, function(err, result) {
+		if(err) callback(err, null);
+		callback(null, result);
+	});
 }
--- routers/test.js
+++ routers/test.js
@@ -1,35 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const getwebdata=require('../middleware/getwebdata');
-const async=require('async');
+const path=require('path');
+const getwebdata = require('../middleware/getwebdata.js');
+const excel2webdata=require('../middleware/excel2webdata.js')
 
-router.get('/',function(req,res,next){
-	async.waterfall([
-		function(callback){
-			var arr=[];
-			var items=['sod35583','sod12408'];
-			async.mapSeries(items,function(item,callback){
-				getwebdata('http://www.dressilyme.com/productsearch.html',{'keyword':'sod35583'},function(err,data){
-					if(err)callback(err,null);
-					arr.push(data);
-					//callback(arr);
-				},function(err,result){
-					callback(null,arr);
-				});
-				
-			});
-//			getwebdata('http://www.dressilyme.com/productsearch.html',{'keyword':'sod35583'},function(err,data){
-//				if(err)callback(err,null);
-//				callback(null,data);
-//			});
-		},
-		function(data,callback){
-			res.json(data);
-		}
-	],function(error, result) {
-		console.log('error: ' + error);
-		res.json(0);
+//const fs = require('fs');
+
+router.get('/', function(req, res, next) {
+	excel2webdata(path.join('./public/upload' + '/test.xlsx'),function(err,result){
+		res.json(result);
 	});
 });
 
-module.exports=router;
+module.exports = router;
